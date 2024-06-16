34f2d1aa7806b265d81584dcd8f510a4782fc997
Andrii <andre40000k@gmail.com>
2024-06-16 12:55:00 +0300

    Add project files.

---
+++ ResumeApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace ResumeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnShowResumeButtonClicked(object sender, EventArgs e)
+        {
+            string resumeText = await LoadResumeAsync();
+            ResumeLabel.Text = resumeText;
+        }
+
+        private async Task<string> LoadResumeAsync()
+        {
+            using (var stream = await FileSystem.OpenAppPackageFileAsync("Resume.txt"))
+            using (var reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+
+}
